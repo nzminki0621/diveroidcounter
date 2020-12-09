d5a3dba2d0a8e26ae39c010127d3ff5fd523b13e
lee.min.k <pz324250@snu.ac.kr>
2020-12-09 14:56:45 +0900

    semifinal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import png from './png.png';
+import logo from './new_brand.png';
 
-function App() {
-  return (
-    <div className="App">
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updatinger = this.updatinger.bind(this);
+  }
+  state = {
+    isLoading: true,
+    
+    
+  }
+
+  getKick = async () => { 
+    
+    fetch(`https://cors-anywhere.herokuapp.com/https://www.kickstarter.com/projects/search.json?search=&term=mobile%20a%20better`)
+    // We get the API response and receive data in JSON format...
+    .then(response => response.json())
+    // ...then we update the users state
+    .then(data =>
+      this.setState({
+        pledge:data.projects[0].pledged,
+        backers: data.projects[0].backers_count,
+        isLoading: false,
+      })
+    )
+
+    console.log("did it");
+  }
+  
+  updatinger(){
+    fetch(`https://cors-anywhere.herokuapp.com/https://www.kickstarter.com/projects/search.json?search=&term=mobile%20a%20better`)
+    // We get the API response and receive data in JSON format...
+    .then(response => response.json())
+    // ...then we update the users state
+    .then(data =>
+      this.setState({
+        pledge:data.projects[0].pledged,
+        backers: data.projects[0].backers_count,
+        isLoading: false,
+      }))
+    console.log("did it too");
+    console.log("good job");
+  }
+
+  componentDidMount(){
+    this.getKick();
+    setInterval(this.getKick, 60000);
+    }
+  
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="divlogo" alt="divlogo" />
+        <img src={png} className="App-logo" alt="logo" />
+        <div className="pledgediv">
+          {this.state.isLoading ? 
+            <span className="pledge">Loading<br/>...</span>:
+            <p className="pledge">
+            $ {String(this.state.pledge)}<br/>{String(this.state.backers)} Backers
+            </p>
+          }
+        </div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://kickstarter.diveroid.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          DIVEROID
         </a>
       </header>
     </div>
-  );
+    );
+  }
 }
-
 export default App;
